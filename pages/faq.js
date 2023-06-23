6974a146f82f9d69e0c6c1aa482f03922654954e
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2023-06-23 01:10:03 +0000

    primeiro commit

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+export default function faq() {
+
+    return (
+        <div>
+            <h1>Pagina do fag</h1>
+            <a href="/">Ir para a home</a>
+        </div>
+
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+
+    return (
+        <div>
+            <h1>Alura Cases - Home page</h1>
+            <a href="/faq">Ir para o FAQ_</a>
+        </div>
+
+    )
+}
