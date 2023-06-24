8d6e5233a18232f462da27635ae2248c936600ec
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2023-06-24 22:09:57 +0000

    componente link

--- pages/faq.js
+++ pages/faq.js
@@ -1,9 +1,12 @@
+
+
 export default function faq() {
 
     return (
         <div>
             <h1>Pagina do fag</h1>
-            <a href="/">Ir para a home</a>
+
+            
         </div>
 
     )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
+import Link from '../src/componentes/Link'
+
 export default function HomePage() {
 
     return (
         <div>
             <h1>Alura Cases - Home page</h1>
-            <a href="/faq">Ir para o FAQ_</a>
-        </div>
+
+            <Link href="/faq">
+                IR PARA A PAGINA DO FAQ
+            </Link>
+         
+        </div>  
 
     )
 }
